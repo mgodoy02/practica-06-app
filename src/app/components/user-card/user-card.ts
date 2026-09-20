617c65d9dbf26c6dc075e2c5cdddcff6f5ea4336
Mariel Godoy Pinpin <marielgodoypinpin@gmail.com>
2026-09-20 18:11:08 +0200

    C-6: He creado el componente UserCard

--- src/app/components/user-card/user-card.ts
+++ src/app/components/user-card/user-card.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IUser } from '../../interfaces/iuser.interface';
+import { Users } from '../../services/users';
+import { toast } from 'ngx-sonner';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-user-card',
   styleUrl: './user-card.css',
   templateUrl: './user-card.html',
 })
-export class UserCard {}
+export class UserCard {
+  myUser = input.required<IUser>();
+  deleted = output<string>();
+  usersService = inject(Users);
+
+  async onDelete(_id: string | undefined) {
+    if (!_id) return;
+
+    const confirmado = confirm('¿Seguro que quieres borrar este usuario?');
+    if (!confirmado) return;
+
+    try {
+      const respuesta = await this.usersService.deleteById(_id);
+      if (respuesta._id) {
+        toast.success('Usuario borrado correctamente');
+        this.deleted.emit(_id);
+      } else {
+        toast.error('No se ha podido borrar el usuario');
+      }
+    } catch (error) {
+      toast.error('Ha ocurrido un error al borrar el usuario');
+      console.log(error);
+    }
+  }
+}
