bd02bdd28d94957b078a130e057e68833745a225
Mariel Godoy Pinpin <marielgodoypinpin@gmail.com>
2026-09-20 18:27:54 +0200

    C-7: Maquetación de la lista de usuarios

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserCard } from '../../components/user-card/user-card';
+import { Users } from '../../services/users';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, UserCard],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home implements OnInit {
+  usersService = inject(Users);
+  usersList = signal<IUser[]>([]);
+  cargando = signal(true);
+  error = signal(false);
+
+  ngOnInit() {
+    this.cargarUsuarios();
+  }
+
+  async cargarUsuarios() {
+    this.cargando.set(true);
+    this.error.set(false);
+    try {
+      const respuesta = await this.usersService.getUsers();
+      this.usersList.set(respuesta.results);
+    } catch (error) {
+      this.error.set(true);
+      console.log(error);
+    } finally {
+      this.cargando.set(false);
+    }
+  }
+
+  onUserDeleted(_id: string) {
+    this.usersList.update(lista => lista.filter(user => user._id !== _id));
+  }
+}
